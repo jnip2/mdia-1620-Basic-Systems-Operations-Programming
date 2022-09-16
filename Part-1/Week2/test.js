d07beaa19b589f94d9039a247ced4a24fbbb24d6
Joey Nip <jnip2@se14l0120-13.ad.bcit.ca>
2022-09-16 14:44:28 -0700

    new item

---
+++ Part-1/Week2/test.js
@@ -0,0 +1,14 @@
+/* 
+DECLARATION:
+Declare a variable called electronic devices 
+*/
+
+//camelCase
+var electronicDevices;
+
+//PascalCase
+var ElectronicDevices;
+
+//Snake_Case
+var Electronic_Devices;
+
