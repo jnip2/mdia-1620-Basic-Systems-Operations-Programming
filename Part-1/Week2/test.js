fa1f76bb3b5f91531024c51eedf8a19542cad8cc
Joey Nip <jnip2@se14l0120-13.ad.bcit.ca>
2022-09-16 15:50:15 -0700

    first lecture

--- Part-1/Week2/test.js
+++ Part-1/Week2/test.js
@@ -12,3 +12,44 @@ var ElectronicDevices;
 //Snake_Case
 var Electronic_Devices;
 
+/* 
+INITIALIZE
+Use camelCase
+*/
+
+//Initialize variage age to a value of 14
+var age = 14;
+
+console.log(age);
+
+//String
+var name = "Joey";
+
+//Boolean
+//Initialize variable called proceed to the value true
+var proceed = true;
+var i = 0;
+
+while(proceed) {
+    i++;
+    proceed = false;
+    console.log(i);
+}
+
+//Objects
+var grannySmith = {
+    colour: "green",
+    originLocation: "australia",
+    originDate: 1868
+}
+
+console.log(grannySmith.colour);
+
+//Function
+var x = myFunction(1, 3);
+
+function myFunction(num1, num2) {
+    return num1*num2;
+}
+
+console.log(x);
