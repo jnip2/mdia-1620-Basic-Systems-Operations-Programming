a78e8ce23f68c717f79caaaed069496b467dcb52
unknown <jnip2@my.bcit.ca>
2022-10-14 14:25:19 -0700

    wrote stuff in files

---
+++ Week6/array/array.js
@@ -0,0 +1,7 @@
+var shoppingList = [];
+
+shoppingList[0] = 'paste for teethies'
+shoppingList[1] = 'potato slices dropped in really hot oil like at least 5 degs'
+shoppingList[2] = 'bred'
+
+console.log(shoppingList)
